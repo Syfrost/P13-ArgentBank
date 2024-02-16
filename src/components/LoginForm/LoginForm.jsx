7d5808d2f5d8f700836b45d3b1e7909a54e01599
Cédric G <wildeyes-dtt@outlook.fr>
2024-02-16 16:01:36 +0100

    ✅ Formulaire de test retiré pour utiliser le composant prévu à l'effet

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,23 +1,81 @@
-import { FormEvent, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { FormEvent, useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faUserCircle} from '@fortawesome/free-solid-svg-icons'
+import {updateToken, updateUser} from "../../store/reducers/UserSlice.js";
+import {addAccount} from "../../store/reducers/AccountSlice.js";
+import './LoginForm.scss';
 
 function LoginForm() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [rememberMe, setRememberMe] = useState(false);
+    const { loading, error, token } = useSelector(state => state.user);
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
-    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    useEffect(() => {
+        if(token !== null) {
+            navigate('/account');
+        }
+    }, [token, navigate]);
+
+    const handleLoginEvent = async (e) => {
         e.preventDefault();
+        const userCredentials = {
+            email: email,
+            password: password
+        }
 
-        dispatch(loginUser(email, password));
+        const response = await fetch('http://localhost:3001/api/v1/user/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(userCredentials)
+        });
+        const data = await response.json();
+
+        if(response.ok) {
+            dispatch(updateToken(data.body.token))
+            console.log('token', data.body.token)
+
+            const userResponse = await fetch('http://localhost:3001/api/v1/user/profile', {
+                method: 'POST',
+                headers: {
+                    Authorization: `Bearer ${data.body.token}`
+                },
+            })
+            const userData = await userResponse.json();
+            console.log('yolo', userData)
+            dispatch(updateUser(userData.body))
+            dispatch(addAccount({ //DEBUG VALEUR EXEMPLE
+                name: 'Argent Bank Checking',
+                total: '$2,082.79',
+                type: 'Available Balance',
+                transactions: 5
+            }));
+            dispatch(addAccount({ //DEBUG VALEUR EXEMPLE
+                name: 'Argent Bank Savings',
+                total: '$10,928.42',
+                type: 'Available Balance',
+                transactions: 10
+            }));
+            dispatch(addAccount({ //DEBUG VALEUR EXEMPLE
+                name: 'Argent Credit Card',
+                total: '$184.30',
+                type: 'Current Balance',
+                transactions: 3
+            }));
+        }
     };
 
     return (
         <section className="sign-in-content">
-            <i className="fa fa-user-circle sign-in-icon"></i>
+            <FontAwesomeIcon className={"sign-in-icon"} icon={faUserCircle} />
             <h1>Sign In</h1>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleLoginEvent}>
                 <div className="input-wrapper">
                     <label htmlFor="username">Username</label>
                     <input
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,17 +1,11 @@
-import React, {useState, useEffect} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {useNavigate} from "react-router-dom";
+import React, {useEffect} from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import "./Login.scss";
-import {updateToken, updateUser} from "../../store/reducers/UserSlice.js";
-import {addAccount} from "../../store/reducers/AccountSlice.js";
+import LoginForm from "../../components/LoginForm/LoginForm.jsx";
 
 export default function Login() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-
-    const {loading, error, token} = useSelector(state => state.user);
-
-    const dispatch = useDispatch();
+    const { token } = useSelector(state => state.user);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -20,69 +14,10 @@ export default function Login() {
         }
     }, [token, navigate]);
 
-    const handleLoginEvent= async (e) => {
-        e.preventDefault();
-        const userCredentials= {
-            email, password
-        }
-
-        const response = await fetch('http://localhost:3001/api/v1/user/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(userCredentials)
-        });
-        const data = await response.json();
-
-        if(response.ok) {
-            dispatch(updateToken(data.body.token))
-            console.log('token', data.body.token)
-
-            const userResponse = await fetch('http://localhost:3001/api/v1/user/profile', {
-                method: 'POST',
-                headers: {
-                    Authorization: `Bearer ${data.body.token}`
-                },
-            })
-            const userData = await userResponse.json();
-            console.log('yolo', userData)
-            dispatch(updateUser(userData.body))
-            dispatch(addAccount({ //DEBUG VALEUR EXEMPLE
-                name: 'Argent Bank Checking',
-                total: '$2,082.79',
-                type: 'Available Balance',
-                transactions: 5
-            }));
-            dispatch(addAccount({ //DEBUG VALEUR EXEMPLE
-                name: 'Argent Bank Savings',
-                total: '$10,928.42',
-                type: 'Available Balance',
-                transactions: 10
-            }));
-            dispatch(addAccount({ //DEBUG VALEUR EXEMPLE
-                name: 'Argent Credit Card',
-                total: '$184.30',
-                type: 'Current Balance',
-                transactions: 3
-            }));
-        }
-
-    }
-
     return (
         <>
             <div className={'login'}>
-                <form className={'form-group'} onSubmit={handleLoginEvent}>
-                    <label htmlFor="email">Email</label>
-                    <input type="email" id="email" name="email" placeholder="Votre email" onChange={(e)=>setEmail(e.target.value)} required/>
-                    <label htmlFor="password">Mot de passe</label>
-                    <input type="password" id="password" name="password" placeholder="Votre mot de passe" onChange={(e)=>setPassword(e.target.value)} required/>
-                    <button type="submit">{loading?'Loading...':'Login'}</button>
-                    {error&&(
-                        <div className={'alert alert-danger'} role='alert'>{error}</div>
-                    )}
-                </form>
+                <LoginForm/>
             </div>
         </>
     )
